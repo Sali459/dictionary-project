93912a825b0cfa619516c3caefa57268da949130
Sali459 <saadiaaali2002@gmail.com>
2026-04-04 17:09:47 +1100

    Rendered the word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,29 +1,33 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css"
+import Results from "./Results";
+import "./Dictionary.css";
 
 export default function Dictionary() {
   // 3.
   let [keyword, setKeyword] = useState("");
+  //7. 
+  let [results, setResults] = useState(null);
 
   // 6.
   function handleResponse(response) {
-    console.log(response.data[0]);
+   console.log(response.data[0]);
+   setResults(response.data[0]);
   }
 
   // 2.
   function search(event) {
     event.preventDefault(); //Prevents the page from reloading
     alert(`Searching for ${keyword} definition`);
-  
+
     //5.
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl)
+    console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
   }
 
   // 4.
-    function handleKeywordChange(event) {
+  function handleKeywordChange(event) {
     setKeyword(event.target.value); //stores the word into a state
   }
 
@@ -33,6 +37,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
