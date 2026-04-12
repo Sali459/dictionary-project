5acba6ef926802df641ac334205c40f153a6f5d1
Sali459 <saadiaaali2002@gmail.com>
2026-04-12 13:37:37 +1000

    Implemented photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -10,12 +11,20 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
   //9.
   let [loaded, setLoaded] = useState(false);
+  //13.
+  let [photos, setPhotos] = useState(null);
 
   // 6.
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
+
+  //13.
+  function handleImageResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
+  }
+
 
   //8.
   function search() {
@@ -23,6 +32,11 @@ export default function Dictionary() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
+
+    //12.
+    let imagesApiKey = "a8boetdc4878122040379fe8f04a70c1";
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}&per_page=6`;
+    axios.get(imagesApiUrl).then(handleImageResponse);
   }
 
   // 2.
@@ -62,6 +76,7 @@ export default function Dictionary() {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
